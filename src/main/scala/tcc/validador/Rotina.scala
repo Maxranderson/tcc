a76717f070c3eb6d177832fedd87be80be6e606e
Maxranderson Araújo <max_rodrigues_araujo@hotmail.com>
2018-04-17 09:50:10 -0300

    Implementado novo metodo para validar em paralelo

--- src/main/scala/tcc/validador/Rotina.scala
+++ src/main/scala/tcc/validador/Rotina.scala
@@ -13,4 +13,8 @@ protected[validador] trait Rotina {
   def validarFromFile[A](file: File, dataCompetencia: Date, unidadeGestoraArquivo: String, controleArquivo: ControleArquivo, gerarTuplaProcessadorEtapas: (MetaDados) => Try[(Conversor[A], Seq[Etapa])]): Try[ResultadosValidacao[A]] = {
     Processadores.processarLinhasSequencial(Source.fromFile(file, "UTF-8").getLines().toList.zipWithIndex, MetaDados("", 0, dataCompetencia, unidadeGestoraArquivo, controleArquivo), gerarTuplaProcessadorEtapas)
   }
+
+  def validarFromFileParalelo[A](file: File, dataCompetencia: Date, unidadeGestoraArquivo: String, controleArquivo: ControleArquivo, gerarTuplaProcessadorEtapas: (MetaDados) => Try[(Conversor[A], Seq[Etapa])]): Try[ResultadosValidacao[A]] = {
+    Processadores.processarLinhasParalelo(Source.fromFile(file, "UTF-8").getLines().toVector.zipWithIndex, MetaDados("", 0, dataCompetencia, unidadeGestoraArquivo, controleArquivo), gerarTuplaProcessadorEtapas)
+  }
 }
