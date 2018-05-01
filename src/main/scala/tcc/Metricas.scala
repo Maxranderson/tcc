f62eeb61087de61f23df32ac1fd90aeaf856c9c1
Maxranderson Araújo <max_rodrigues_araujo@hotmail.com>
2018-05-01 16:23:14 -0300

    Criado a microbenchmark de tempo de execução

--- src/main/scala/tcc/Metricas.scala
+++ src/main/scala/tcc/Metricas.scala
@@ -1,6 +1,9 @@
 package tcc
 
+import java.io.File
+
 object Metricas {
+
   def tempoExecucaoComResultado[R](bloco: => R): R = {
     val t0 = System.currentTimeMillis()
     val result = bloco
@@ -8,4 +11,30 @@ object Metricas {
     println(s"O tempo foi: ${t1-t0} ms")
     result
   }
+
+  def tempoExecucaoComResultados[R](bloco: => R): (R, Long) = {
+    val t0 = System.currentTimeMillis()
+    val result = bloco
+    val t1 = System.currentTimeMillis()
+    (result, t1-t0)
+  }
+
+  def tempoExecucaoPorArquivoEhQuantidadeLinha[R](bloco: (File) => R): String = {
+    val quantidade = 1 to 15
+    val arquivos = quantidade.map(vez => new File(s"src/main/resources/arquivos-testes/vezes$vez.txt"))
+    val resultados = arquivos.map {
+      arquivo =>
+        Metricas.tempoExecucaoComResultados {
+          bloco(arquivo)
+        }
+    }
+
+    val resultadosPorLinha = resultados.zipWithIndex.foldLeft("") {
+      (acumulador, proximo) =>
+        val ((res, tempo), indice) = proximo
+        acumulador + "\n" + s"Resultado de ${(indice + 1) * 400} linhas: $tempo ms"
+    }
+   resultadosPorLinha + "\n" + s"A média foi: ${resultados.foldLeft(0.toDouble)(_ + _._2) / resultados.length} ms"
+  }
+
 }
